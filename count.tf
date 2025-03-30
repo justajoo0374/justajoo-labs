cc4b6cf2c73235f8ffecd3b6a76bd3353cf415d0
justajoo0374 <justajoo0374@gmail.com>
2025-03-30 12:47:51 +0530

    Update count.tf

--- count.tf
+++ count.tf
@@ -1,9 +1,9 @@
-resource "google_compute_instance" "count" {
-  name         = "count"
+resource "google_compute_instance" "counts" {
+  count = 3
+  name         = "vm-${count.index}"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
   project = "magnetic-flare-454316-h0"
-  count = 3
   boot_disk {
     initialize_params {
       image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"

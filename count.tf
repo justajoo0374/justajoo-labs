623c22532af3f9aaaff536de6167ab30cb5c1c7b
justajoo0374 <justajoo0374@gmail.com>
2025-03-30 12:37:53 +0530

    coaunt-3-vm

--- count.tf
+++ count.tf
@@ -1,5 +1,5 @@
-resource "google_compute_instance" "vm1" {
-  name         = "test1"
+resource "google_compute_instance" "count" {
+  name         = "count"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
   project = "magnetic-flare-454316-h0"

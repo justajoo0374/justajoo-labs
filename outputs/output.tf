f56caa93780205ca6afc867f35b97df94059509c
justajoo0374 <justajoo0374@gmail.com>
2025-03-30 07:36:48 +0530

    Create output.tf

---
+++ outputs/output.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+output "ip" {
+  value = google_compute_instance.default.network_interface.0.network_ip
+}

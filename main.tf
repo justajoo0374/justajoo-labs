f021c009e027cfb557ce4a3eb27271b9fba43fe9
justajoo0374 <justajoo0374@gmail.com>
2025-03-30 08:48:41 +0530

    putput-value

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
-module "output" {
-  source = "./outputs/"
+resource "google_compute_instance" "vm1" {
+  name         = "vm1"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+  project = "magnetic-flare-454316-h0"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
+/*output "ip" {
+  value = google_compute_instance.vm1.network_interface.0.network_ip
+}*/
+output "ip" {
+  value = "google_compute_instance.vm1"
 }

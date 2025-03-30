d3da242c70a4969d7f2fc8e15cbe8dc961bc2524
justajoo0374 <justajoo0374@gmail.com>
2025-03-30 09:04:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ resource "google_compute_instance" "vm" {
 /*output "ip" {
   value = google_compute_instance.vm1.network_interface.0.network_interface
 }*/
-output "vm-atributes" {
-  value = google_compute_instance.vm.network_interface.0.network_interface
+output "ip" {
+  value = "google_compute_instance.vm.network_interface.0.network_interface"
 }

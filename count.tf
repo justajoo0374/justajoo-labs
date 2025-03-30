407a775889f3b2f1eb2f10046a38a8d9db6dc4de
justajoo0374 <justajoo0374@gmail.com>
2025-03-30 12:35:33 +0530

    count-3

---
+++ count.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "vm1" {
+  name         = "test1"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+  project = "magnetic-flare-454316-h0"
+  count = 3
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}

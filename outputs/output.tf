2d16bbbb5dde56fb52b57febe6fabe9d8409aea0
justajoo0374 <justajoo0374@gmail.com>
2025-03-30 07:58:42 +0530

    Update output.tf

--- outputs/output.tf
+++ outputs/output.tf
@@ -1,26 +1,16 @@
-provider "google" {
-    project ="project = "magnetic-flare-454316-h0"
-}
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
+resource "google_compute_instance" "vm" {
+  name         = "vm"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
   project = "magnetic-flare-454316-h0"
+
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
     }
   }
+
   network_interface {
     network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
   }
-output "ip" {
-  value = google_compute_instance.default.network_interface.0.network_ip
 }

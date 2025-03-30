770e220fee2c2ab6065b6fd0955a7696fc8a4306
justajoo0374 <justajoo0374@gmail.com>
2025-03-30 09:08:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_compute_instance" "vm" {
-  name         = "test"
+resource "google_compute_instance" "vm1" {
+  name         = "test1"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
   project = "magnetic-flare-454316-h0"
@@ -12,9 +12,9 @@ resource "google_compute_instance" "vm" {
     network = "default"
   }
 }
-/*output "ip" {
-  value = google_compute_instance.vm1.network_interface.0.network_interface
-}*/
 output "ip" {
-  value = "google_compute_instance.vm.network_interface.0.network_interface"
+  value = google_compute_instance.vm1.network_interface.0.network_interface
 }
+/*output "ip" {
+  value = google_compute_instance.vm.network_interface.0.network_interface
+}*/

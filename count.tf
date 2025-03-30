c34ea20b4a333d3c8465a9bb60b0c4aadde48455
justajoo0374 <justajoo0374@gmail.com>
2025-03-30 13:23:01 +0530

    count-unique-name

--- count.tf
+++ count.tf
@@ -1,3 +1,4 @@
+#create vm using count and count.index , it will create with number (like 0, 1,2...)
 /*resource "google_compute_instance" "count" {
   count = 3
   name         = "vm-${count.index}"
@@ -13,3 +14,24 @@
     network = "default"
   }
 }*/
+
+
+#create vm with clout with unique name insteda of just adding number like(0,1,2)
+resource "google_compute_instance" "count" {
+  count = 2
+  name         = "var.instance[count.index]"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+  project = "magnetic-flare-454316-h0"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+varibale "instance" {
+   default = ["dev", "prod"]
+   type =list
+}

f613c8f076baef6139dd9f9ce857af6e3f2025a4
justajoo0374 <justajoo0374@gmail.com>
2025-03-30 07:47:08 +0530

    project id added

--- outputs/output.tf
+++ outputs/output.tf
@@ -2,6 +2,7 @@ resource "google_compute_instance" "default" {
   name         = "my-instance"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
+  project = "magnetic-flare-454316-h0"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
